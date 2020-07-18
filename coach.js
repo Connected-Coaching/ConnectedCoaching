edac3cc651b2c27b642d9343d0587f5b9a107041
Wyatt Cowley <wycowley@gmail.com>
2020-07-18 10:24:04 -0700

    trying to make it easy to add classes

--- coach.js
+++ coach.js
@@ -1,6 +1,23 @@
+function startedUp(){
+    createClassCard();
 
-
+}
+// In progress for adding everything 
 function createClassCard(){
+    var days = ["monday","tuesday"]
+
     var outerDiv = document.createElement("div");
-    outerDiv.id = 
+    outerDiv.id = "outerClassDiv";
+    var innerDiv = document.createElement("div");
+    innerDiv.style.width = "80%"
+    innerDiv.style.marginLeft = "10%"
+    var title = document.createElement("h1");
+    title.innerHTML = "Put name here"
+    var floatedDiv = document.createElement("div");
+    floatedDiv.style.width = "100%";
+    var 
+
+    innerDiv.appendChild(title)
+    outerDiv.appendChild(innerDiv);
+    document.getElementById("startPlacingHere").appendChild(outerDiv)
 }
