7427794a87ec815d8f6c608bc5500c749df5e6ab
Wyatt Cowley <wycowley@gmail.com>
2020-07-19 11:22:54 -0700

    add stuff

--- public/athlete.js
+++ public/athlete.js
@@ -1,10 +1,152 @@
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+    apiKey: 'AIzaSyCH1DEQxf17qHLYz5Bf_H0zmlX9VmWUmP0',
+    authDomain: 'connected-coaching-283705.firebaseapp.com',
+    projectId: 'connected-coaching-283705'
+});
+  
+var db = firebase.firestore();
+
+//gets called after page load
 function startedUp(){
-    createClassCard("Coach Raffi's Hell",["Monday","Tuesday","Wednesday","Thursday","Friday"]);
+    window.alert(document.cookie)
+
+    //makes sure that the person has been added to the database
+    var addUsername = db.collection('users').doc(getCookie("email"));
+    var setWithMerge = addUsername.set({
+        "name":getCookie("displayName"),
+        // make sure to change this to false for students
+        "coach":true
+    }, { merge: true });
+
+    console.log("doing stuff")
+    //draws each one of the classes that are pertinent to the coach
+    db.collection("users").doc(getCookie("email")).collection("classes")
+    .get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+            var assignments = []
+            db.collection("classes").doc(doc.id).collection("assignments")
+            .get()
+            .then(function(querySnapshot) {
+                querySnapshot.forEach(function(doc2) {
+                    // doc.data() is never undefined for query doc snapshots
+                    console.log(doc2.id, " => ", doc2.data());
+                    assignments.push(doc2.id)
+                });
+                console.log(assignments)
+                console.log(doc.data.name)
+                createClassCard(doc.id,assignments)
+
+            })
+            .catch(function(error) {
+                console.log("Error getting documents: ", error);
+            });
+        });
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
+
+
 
 }
 // In progress for adding everything 
-function createClassCard(titleText, days){
 
+// you can just put in the name of the cookie you want to get (like "email"), and it will return what it is
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+function makeInvisible(){
+    
+}
+  //to avoid errors with arrays not being well supported, I created another collection (and another document)
+  //adds it into user database
+  function addClass(){
+    var createTheClass = db.collection('users').doc(getCookie("email")).collection("classes").doc(document.getElementById("nameOfClass").value);
+    createTheClass.get()
+    .then((docSnapshot) => {
+    if (docSnapshot.exists) {
+      createTheClass
+        .onSnapshot((doc) => {
+            console.log("class exists");
+
+            createTheClass.set({
+            "name":document.getElementById("nameOfClass").value,
+            "created":true
+            }).then(function() {
+                console.log("Document successfully written!");
+
+                var assignments = []
+                db.collection("classes").doc(document.getElementById("nameOfClass").value).collection("assignments")
+                .get()
+                .then(function(querySnapshot) {
+                    querySnapshot.forEach(function(doc) {
+                        // doc.data() is never undefined for query doc snapshots
+                        console.log(doc.id, " => ", doc.data());
+                        assignments.push(doc.id)
+                    });
+                    console.log(assignments)
+                    createClassCard(document.getElementById("nameOfClass").value,assignments)
+    
+                })
+                .catch(function(error) {
+                    console.log("Error getting documents: ", error);
+                });
+            })
+            .catch(function(error) {
+                console.error("Error writing document: ", error);
+            });
+            
+        });
+    }else{
+        window.alert("That class does not exist")
+    }
+  });
+
+    createTheClass.set({
+        "name":document.getElementById("nameOfClass").value,
+        "created":true
+    }).then(function() {
+        console.log("Document successfully written!");
+    })
+    .catch(function(error) {
+        console.error("Error writing document: ", error);
+    });
+
+
+    //adds the class to the other root
+
+    console.log("Hello?")
+    console.log(document.getElementById("nameOfClass").value+"")
+    
+    
+  }
+function addActivity(className){
+
+}
+  function createClassCard(titleText, days){
+    var outerButton = document.createElement("button");
+    outerButton.style.width = "100%"
+    outerButton.onclick = function(){
+        window.location.href = "/addActivity.html?class="+titleText;
+    }
+    console.log("addActivity(\""+titleText+"\")")
+    outerButton.style.border = "none";
 
     var outerDiv = document.createElement("div");
     outerDiv.id = "outerClassDiv";
@@ -28,33 +170,32 @@ function createClassCard(titleText, days){
     var eachTitleDiv = document.createElement("div");
     eachTitleDiv.style.float = "left";
     eachTitleDiv.style.width = "60%"
-    for(var i = 0;i<days.length && i<3;i++){
-        var eachTitle = document.createElement("h2");
-        eachTitle.innerHTML = days[i];
-        eachTitle.style.margin = "0%";
-        eachTitle.style.width = "52%"
-        eachTitle.style.float = "left"
-        eachTitleDiv.appendChild(eachTitle)
+    for(var i = 0;i<3;i++){
+        if(i<days.length){
+            var eachTitle = document.createElement("h2");
+            eachTitle.innerHTML = days[i];
+            eachTitle.style.margin = "0%";
+            eachTitle.style.width = "52%"
+            eachTitle.style.float = "left"
+            eachTitleDiv.appendChild(eachTitle)
+        }else{
+            var eachTitle = document.createElement("h2");
+            eachTitle.innerHTML = "";
+            eachTitle.appendChild(document.createElement("br"))
+            eachTitle.style.margin = "0%";
+            eachTitle.style.width = "52%"
+            eachTitle.style.float = "left"
+            eachTitleDiv.appendChild(eachTitle)
+        }
     }
     floatedDiv.appendChild(eachTitleDiv)
 
 
-    var button = document.createElement("button");
-    var img = document.createElement("img")
-    img.src = "Images/add.png";
-    button.style.width = "25%";
-    button.style.position = "relative";
-    // button.style.left = "50%";
-    // button.style.top = "-20%"
-    button.style.float  = "left";
-    img.className = "plusImage";
-    button.className = "addButton";
     
-    button.appendChild(img)
-    floatedDiv.appendChild(button);
 
     innerDiv.appendChild(title);
     innerDiv.appendChild(floatedDiv);
     outerDiv.appendChild(innerDiv);
-    document.getElementById("startPlacingHere").appendChild(outerDiv)
+    outerButton.appendChild(outerDiv);
+    document.getElementById("startPlacingHere").append(outerButton)
 }
